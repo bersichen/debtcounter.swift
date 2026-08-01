052a5a42ebf5c4c2414d6d313892e3300de42d07
Michael Nikitochkin <michael.nikitochkin@gmx.net>
2026-08-01 14:56:06 +0200

    created Test record debugging

--- DebtCounter/DebtCounterApp.swift
+++ DebtCounter/DebtCounterApp.swift
@@ -3,10 +3,21 @@ import SwiftData
 
 @main
 struct DebtCounterApp: App {
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([Payment.self])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+    do {
+      return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
+    }
+  }()
+
   var body: some Scene {
     WindowGroup {
       SwiftUIView()
-        .modelContainer(for: Payment.self)
     }
+    .modelContainer(sharedModelContainer)
   }
 }
